8e13871389c5069f92f9b6463db5eae3b42200a7
gaoyang836 <781653664@qq.com>
2019-02-19 18:08:31 +0800

    map

--- mapreduce.py
+++ mapreduce.py
@@ -44,3 +44,53 @@
 # for n in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
 #     L.extend({f(n}])
 # print(L)
+
+#list转化为字符串
+# print(list(map(str,[1,2,3,4,5,6,7,8,9])))
+
+#reduce
+# from functools import reduce
+# def add(x,y):
+#     return x+y
+# print(reduce(add,[1,3,5,7,9]))
+
+# from functools import reduce
+# # def fn(x,y):
+# #     return x*10+y
+# # char2num函数中参数s,按照键找到对应值返回
+# #(通过map把一个整的字符串迭代单个的字符串）
+# def char2num(s):
+#     digits = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+#     print(digits[s])
+#     return digits[s]
+# # print(reduce(fn,map(char2num,'13579')))
+# print(map(char2num,'13579'))
+
+# from functools import reduce
+# DIGITS = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+# def char2int(s):
+#     def fn(x,y):
+#         return x*10+y
+#     def char2num(s):
+#         return DIGITS[s]
+#     return reduce(fn, map(char2num, s))
+
+#练习
+
+#利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字
+# def normalize(name):
+#     return name.capitalize()
+# L1 = ['adam', 'LISA', 'barT']
+# L2 = list(map(normalize, L1))
+# print(L2)
+
+#
+from functools import reduce
+L=[3, 5, 7, 9]
+def prod(L):
+    def fn(x,y):
+        return x*y
+    def add(L):
+        return sum(L)  
+    return reduce(fn,map(add,L))
+
