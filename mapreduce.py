36159b169b6d97d49ce63a72091346133e73b3f3
gaoyang836 <781653664@qq.com>
2019-02-20 09:45:32 +0800

    map

--- mapreduce.py
+++ mapreduce.py
@@ -84,13 +84,40 @@
 # L2 = list(map(normalize, L1))
 # print(L2)
 
-#
-from functools import reduce
-L=[3, 5, 7, 9]
-def prod(L):
-    def fn(x,y):
-        return x*y
-    def add(L):
-        return sum(L)  
-    return reduce(fn,map(add,L))
+#编写一个prod()函数，可以接受一个list并利用reduce()求积
+# from functools import reduce
 
+# def prod(x,y):
+#     return x*y 
+# print(reduce(prod,[3, 5, 7, 9]))
+
+#编写一个prod()函数，可以接受一个list并利用reduce()求积
+# from functools import reduce
+
+# def prod(L):
+#     def fn(x,y):
+#         return x*y 
+#     return reduce(fn,L)
+
+# print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+# if prod([3, 5, 7, 9]) == 945:
+#     print('测试成功!')
+# else:
+#     print('测试失败!')
+
+# 利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+from functools import reduce
+
+def str2float(s):
+    def fn(x,y):
+        return x*10+y
+    n=s.index('.')
+    s1=list(map(int,[x for x in s[:n]]))
+    s2=list(map(int,[x for x in s[n+1:]]))
+    return reduce(fn,s1)+reduce(fn,s2)/(10**len(s2))#乘幂
+
+print('str2float(\'123.456\') =', str2float('123.456'))
+if abs(str2float('123.456') - 123.456) < 0.00001:
+    print('测试成功!')
+else:
+    print('测试失败!')
