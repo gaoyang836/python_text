28a9e1a7ab7952b56add9d8111df842d24c6b6b2
gaoyang836 <781653664@qq.com>
2019-02-21 09:46:46 +0800

    sorted

--- sorted.py
+++ sorted.py
@@ -17,12 +17,25 @@
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 def by_name(t):
     return t[0][:]
+    return t[0]
 L2 = sorted(L, key=by_name)
 print(L2)
 
-#再按成绩从高到低排序
+# #再按成绩从高到低排序
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 def by_score(t):
     return t[:][1]
+    return t[1]
 L2 = sorted(L, key=by_score)
 print(L2)
+
+
+# # 二维列表的元素都是列表，想获取元素需要取两次索引
+# list = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+# print(list[0])
+# print(list[0][0])
+# print(list[0][:])
+# print(list[1][:])
+# print(list[2][:])
+# print(list[:][1])
+# print(list[:][2])
