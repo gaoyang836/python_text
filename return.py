d45f0c99c98c88ae1855069fb09c48b4482974a9
gaoyang836 <781653664@qq.com>
2019-03-01 17:36:42 +0800

    return

--- return.py
+++ return.py
@@ -20,14 +20,52 @@
 # print(f())
 
 #闭包
-def count():
-    fs=[]
-    for i in range(1,4):
-        def f():
-            return i*i
-        fs.append(f)
-    return fs
-f1,f2,f3=count()
-print(f1())
-print(f2())
-print(f3())
+# def count():
+#     fs=[]
+#     for i in range(1,4):
+#         def f():
+#             return i*i
+#         fs.append(f)
+#     return fs
+# f1,f2,f3=count()
+# print(f1())
+# print(f2())
+# print(f3())
+
+# def count():
+#     def f(j):
+#         def g():
+#             return j*j
+#         return g
+#     fs=[]
+#     for i in range(1,4):
+#         fs.append(f(i))
+#     return fs 
+# f1,f2,f3=count()
+# print(f1())
+# print(f2())
+# print(f3())
+
+#练习
+# def createCounter():
+#     s=[0]
+#     def counter():
+#         s[0]=s[0]+1
+#         return s[0]
+#     return counter
+
+# def createCounter():
+#     s=0
+#     def counter():
+#         nonlocal s
+#         s= s + 1
+#         return s
+#     return counter
+
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('测试通过!')
+else:
+    print('测试失败!')
